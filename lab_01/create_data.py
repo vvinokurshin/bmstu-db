199e7063e1385e03d3d589bb320adce0b21112df
vvs20u384 <valera_valera03@mail.ru>
2022-09-19 00:34:50 +0300

    modifed

--- lab_01/create_data.py
+++ lab_01/create_data.py
@@ -7,20 +7,20 @@ fake = Faker(locale='ru_Ru')
 
 def generate_managers():
     with open('managers.csv', 'w') as f:
-        f.write('id;surname;name;patronymic;citizenship;date_of_birth\n')
-        for id in range(1, 1001):
+        f.write('surname;name;patronymic;citizenship;date_of_birth\n')
+        for _ in range(1, 1001):
             name, patronymic, surname = RussianNames().get_person().split()
             date_of_birth = fake.date_between_dates(datetime.date(1972, 1, 1), datetime.date(2000, 1, 1))
             citizenship = fake.country()
-            f.write(f'{id};{surname};{name};{patronymic};{citizenship};{date_of_birth}\n')
+            f.write(f'{surname};{name};{patronymic};{citizenship};{date_of_birth}\n')
 
 def generate_brands():
     with open('brands.csv', 'w') as f:
-        f.write('id;name_company;country;date_of_create;id_manager\n')
+        f.write('name_company;country;date_of_create;id_manager\n')
         ids_managers = [i for i in range(1, 1001)]
         count = len(ids_managers)
 
-        for id in range(1, 1001):
+        for _ in range(1, 1001):
             name_company = fake.company()
             country = fake.country()
             date_of_create = fake.date_between_dates(datetime.date(1900, 1, 1), datetime.date(2020, 1, 1)).year
@@ -28,43 +28,43 @@ def generate_brands():
             id_manager = ids_managers[ind]
             ids_managers.pop(ind)
             count -= 1
-            f.write(f'{id};{name_company};{country};{date_of_create};{id_manager}\n')
+            f.write(f'{name_company};{country};{date_of_create};{id_manager}\n')
 
 def generate_shops():
     with open('shops.csv', 'w') as f:
-        f.write('id;name;specialization;number_of_employees;profit;id_sc\n')
+        f.write('name;specialization;number_of_employees;profit;id_sc\n')
         specializations = ['Мужское белье', 'Женское белье', 'Детския одежда', 'Мужская одежда', 'Женская одежда',
                            'Спортивная одежда', 'Летняя одежда', 'Зимняя одежда', 'Демисезонная одежда']
 
-        for id in range(1, 1001):
+        for _ in range(1, 1001):
             name = fake.word().title()
             specialization = specializations[r.randint(0, len(specializations)) - 1]
             number_of_employees = r.randint(100, 10000)
             profit = r.randint(1000, 10000000)
             id_sc = r.randint(1, 1000)
-            f.write(f'{id};{name};{specialization};{number_of_employees};{profit};{id_sc}\n')
+            f.write(f'{name};{specialization};{number_of_employees};{profit};{id_sc}\n')
 
 def generate_sc():
     with open('sc.csv', 'w') as f:
-        f.write('id;name;address;number_of_floors;number_of_shops\n')
+        f.write('name;address;number_of_floors;number_of_shops\n')
 
-        for id in range(1, 1001):
+        for _ in range(1, 1001):
             name = fake.word().title()
             address = fake.address().replace(',', '')
             number_of_floors = r.randint(1, 20)
             number_of_shops = number_of_floors * r.randint(10, 20)
-            f.write(f'{id};{name};{address};{number_of_floors};{number_of_shops}\n')
+            f.write(f'{name};{address};{number_of_floors};{number_of_shops}\n')
 
 def generate_owners_sc():
     with open('owners_sc.csv', 'w') as f:
-        f.write('id;surname;name;patronymic;citizenship;date_of_birth;id_sc\n')
+        f.write('surname;name;patronymic;citizenship;date_of_birth;id_sc\n')
 
-        for id in range(1, 1001):
+        for _ in range(1, 1001):
             name, patronymic, surname = RussianNames().get_person().split()
             date_of_birth = fake.date_between_dates(datetime.date(1972, 1, 1), datetime.date(2000, 1, 1))
             id_sc = r.randint(1, 1000)
             citizenship = fake.country()
-            f.write(f'{id};{surname};{name};{patronymic};{citizenship};{date_of_birth};{id_sc}\n')
+            f.write(f'{surname};{name};{patronymic};{citizenship};{date_of_birth};{id_sc}\n')
 
 def generate_links_brands_n_shops():
     with open('links_brands_n_shops.csv', 'w') as f:
--- lab_01/load_data.sql
+++ lab_01/load_data.sql
@@ -1,25 +1,25 @@
 -- Генеральные директора
-COPY public.general_managers(id,surname,name,patronymic,citizenship,date_of_birth)
+COPY public.general_managers(surname,name,patronymic,citizenship,date_of_birth)
 FROM '/home/data/managers.csv' 
 DELIMITER ';' csv HEADER;
 
 -- Фирмы
-COPY public.brands(id,name_company,country,year_of_create,id_manager)
+COPY public.brands(name_company,country,year_of_create,id_manager)
 FROM '/home/data/brands.csv' 
 DELIMITER ';' csv HEADER;
 
 -- ТЦ
-COPY public.sc(id,name,address,number_of_floors,number_of_shops)
+COPY public.sc(name,address,number_of_floors,number_of_shops)
 FROM '/home/data/sc.csv' 
 DELIMITER ';' csv HEADER;
 
 -- Магазины
-COPY public.shops(id,name,specialization,number_of_employees,profit,id_sc)
+COPY public.shops(name,specialization,number_of_employees,profit,id_sc)
 FROM '/home/data/shops.csv' 
 DELIMITER ';' csv HEADER;
 
 -- Владельцы ТЦ
-COPY public.owners_sc(id,surname,name,patronymic,citizenship,date_of_birth,id_sc)
+COPY public.owners_sc(surname,name,patronymic,citizenship,date_of_birth,id_sc)
 FROM '/home/data/owners_sc.csv' 
 DELIMITER ';' csv HEADER;
 
